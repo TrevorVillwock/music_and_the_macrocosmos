d3b58d7782bd2dc139357215425339a7a1185156
Trevor Villwock <trevorvillwock@gmail.com>
2022-08-19 15:40:49 -0500

    made into class, broke button

--- hello_pizz.js
+++ hello_pizz.js
@@ -1,21 +1,40 @@
-function play(freq) {
-    console.log("playing")
-    var sawWave = new Pizzicato.Sound({ 
-        source: 'wave', 
-        options: {
-            type: 'sawtooth',
-            frequency: freq
+
+class HelloSound {
+    
+    constructor() {
+        this.voice1 = {
+            pitches: [60],
+            velocities: [100],
+            durations: [1]
         }
-    });
+        this.instrument = new Pizzicato.Sound({ 
+            source: 'wave', 
+            options: {
+                type: 'sawtooth',
+                frequency: freq
+            }
+        });
+        this.button = document.getElementById("test");
 
-    sawWave.attack = 0.5;
-    sawWave.release = 1;
+        console.log(this.button)
+        
+        this.button.addEventListener('click', () => {
+            console.log("event listener")
+            play(220);
+            this.instrument.stop()
+        });
+    }
 
-    sawWave.play();
+    play(freq) {
+        console.log("playing") 
+
+        this.instrument.attack = 0.5;
+        this.instrument.release = 1;
+
+        this.instrument.play(freq);
+    }
+
+    stop() {
+        this.instrument.stop();
+    }
 }
-
-var button = document.getElementById("test");
-
-button.addEventListener('click', () => {
-    play(220);
-});
