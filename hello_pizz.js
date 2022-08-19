fca28452f903b596c6ead11a1acbcf23a6b301a5
Trevor Villwock <trevorvillwock@gmail.com>
2022-08-19 16:00:14 -0500

    troubleshooting class

--- hello_pizz.js
+++ hello_pizz.js
@@ -1,7 +1,7 @@
 
 class HelloSound {
     
-    constructor() {
+    constructor(freq) {
         this.voice1 = {
             pitches: [60],
             velocities: [100],
@@ -20,21 +20,44 @@ class HelloSound {
         
         this.button.addEventListener('click', () => {
             console.log("event listener")
-            play(220);
+            this.play(220)
             this.instrument.stop()
         });
     }
 
     play(freq) {
         console.log("playing") 
+        
+        var test_instrument = new Pizzicato.Sound({ 
+            source: 'wave', 
+            options: {
+                type: 'sawtooth',
+                frequency: freq
+            }
+        });
 
+        test_instrument.play(freq);
+        /*
         this.instrument.attack = 0.5;
         this.instrument.release = 1;
 
         this.instrument.play(freq);
+        */
     }
 
     stop() {
         this.instrument.stop();
     }
 }
+
+let user_sound = new HelloSound(220);
+
+let outside_instrument = new Pizzicato.Sound({ 
+    source: 'wave', 
+    options: {
+        type: 'sawtooth',
+        frequency: 300
+    }
+});
+
+outside_instrument.play(220);
