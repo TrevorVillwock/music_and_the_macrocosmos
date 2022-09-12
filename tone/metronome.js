06a40fb4a587fe934b099316cd8182166ab3ae21
Trevor Villwock <trevorvillwock@gmail.com>
2022-09-12 17:43:39 -0500

    adds random speed button

--- tone/metronome.js
+++ tone/metronome.js
@@ -1,8 +1,10 @@
-var player = new Tone.Player(
+let player = new Tone.Player(
     "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1506195/keyboard-key.mp3"
 ).toDestination();
 
-Tone.Transport.bpm.value = 120;
+let random = 0;
+
+Tone.Transport.bpm.value = 100;
   
 Tone.Transport.scheduleRepeat((time) => {
     player.start(time).stop(time + 0.1);
@@ -15,5 +17,20 @@ function start() {
 
 function stop() {
     Tone.Transport.stop();
+    random = 0;
 }
-  
+
+async function toggleRandom() {
+    console.log("toggling random");
+    if (random) {
+        random = 0;
+    } else {
+        random = 1;
+    }
+
+    while (random) {
+        Tone.Transport.bpm.value = 150 + Math.random() * 1000; 
+        console.log("bpm: " + Tone.Transport.bpm.value);
+        await new Promise(r => setTimeout(r, (1000*(60 / Tone.Transport.bpm.value))));
+    }
+}
