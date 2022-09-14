0ee0bef1c9b0cc7509250c08bdd9f4e8d7726f97
Trevor Villwock <trevorvillwock@gmail.com>
2022-09-13 20:12:20 -0500

    adds volume control

--- tone/metronome.js
+++ tone/metronome.js
@@ -1,33 +1,47 @@
 // Adapted from https://codepen.io/dirkk0/pen/yLMaMGa
-// This code creates a metronome with a "random speed" button
+// This code creates a metronome with a "random speed" feature
 // that will make it either speed up or slow down by a random
-// amount on each click
+// amount on each click.
 
 // let player = new Tone.Player(
 //     "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1506195/keyboard-key.mp3"
 // ).toDestination();
 
+let volSlider;
+
+window.onload = function () {
+    volSlider = document.getElementById("volumeSlider");
+    console.log(volSlider);
+}
+
+let vol = new Tone.Volume(-30).toDestination()
+
+// Create new Player object and load sound file into it
 let player = new Tone.Player(
     "wood_block.wav"
-).toDestination();
+).connect(vol);
 
-// boolean variable indicating whether randomness is turned on or off
+
+
+// Boolean variable indicating whether randomness is turned on or off
 let random = 0;
 
-Tone.Transport.bpm.value = 100;
+// Set default value for BPM (beats per minute). 60 BPM is one beat per second.
+Tone.Transport.bpm.value = 60;
 
 Tone.Transport.scheduleRepeat((time) => {
     player.start(time).stop(time + 0.1);
 }, "4n");
-  
+
+// Run when start button is clicked
 function start() {
     Tone.start();
     Tone.Transport.start();
 }
 
+// Run when the stop button is clicked
 function stop() {
     Tone.Transport.stop();
-    random = 0;
 }
 
 async function toggleRandom() {
@@ -43,4 +57,16 @@ async function toggleRandom() {
         console.log("bpm: " + Tone.Transport.bpm.value);
         await new Promise(r => setTimeout(r, (1000*(60 / Tone.Transport.bpm.value))));
     }
+}
+
+function setVolume() {
+    // The html slider gives us values 0-200, which we map
+    // to be between -100 and 0 dB because that's what the
+    // Tone.js Volume object expects.
+    // For an explanation of how decibels work see here:
+    // https://ehomerecordingstudio.com/decibels/
+    console.log("volume: " + volSlider.value)
+    let dB = -1 * (200 - volSlider.value)
+    console.log("dB: " + dB)
+    vol.volume.value = -1 * (200 - volSlider.value) 
 }
